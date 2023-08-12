aeb58d649a5806350587429bbacc03e7b8e06bbd
Rojer Du <rojerdu.dev@gmail.com>
2023-08-11 23:45:52 -0700

    Add db list of users

--- main.py
+++ main.py
@@ -1,23 +1,60 @@
-from fastapi import FastAPI
+from typing import List
+from uuid import uuid4
+
 import uvicorn
+from fastapi import FastAPI
+
+from models import Gender, Role, User
 
 app = FastAPI()
+db: List[User] = [
+    User(
+        id=uuid4(),
+        first_name="Thomas",
+        last_name="Anderson",
+        gender=Gender.male,
+        roles=[Role.user],
+    ),
+    User(
+        id=uuid4(),
+        first_name="Elizabeth",
+        last_name="Carter",
+        gender=Gender.female,
+        roles=[Role.user],
+    ),
+    User(
+        id=uuid4(),
+        first_name="Simon",
+        last_name="Bennet",
+        gender=Gender.male,
+        roles=[Role.user],
+    ),
+    User(
+        id=uuid4(),
+        first_name="Gabrielle",
+        last_name="Taylor",
+        gender=Gender.female,
+        roles=[Role.user]
+    ),
+    User(
+        id=uuid4(),
+        first_name="Michael",
+        last_name="Davis",
+        gender=Gender.male,
+        roles=[Role.admin, Role.user],
+    ),
+]
+
 
 @app.get("/")
 async def root():
     return {"greeting": "Hello World!"}
 
 
+@app.get("/api/v1/users")
+async def get_users():
+    return db
 
 
-
-
-
-
-
-
-
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
